228c0579378c8fa51e7092da04acde7a27109e5c
Evdokimov27 <mr.evdokimov27@yandex.ru>
2023-05-01 19:34:16 +0800

    Final Test Build

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -7,6 +7,7 @@ public class Win : MonoBehaviour
 {
 	[SerializeField] private List<GameObject> win_level;
 	[SerializeField] public int for_win;
+	[SerializeField] public GameObject[] light;
 	[SerializeField] private GameObject[] block;
 
 	void Start()
@@ -25,7 +26,18 @@ public class Win : MonoBehaviour
     {
 		if(for_win== 0)
 		{
-			SceneManager.LoadScene(0);
+			for (int i = 0; i < light.Length; i++)
+			{
+				light[i].GetComponent<MeshRenderer>().material.color = Color.Lerp(Color.white, Color.blue, Mathf.Abs(Mathf.Sin(Time.time)));
+			}
+			//SceneManager.LoadScene(0);
+		}
+		else
+		{
+			for (int i = 0; i < light.Length; i++)
+			{
+				light[i].GetComponent<MeshRenderer>().material.color = new Color(0, 121, 204, 255);
+			}
 		}
 	}
 }
